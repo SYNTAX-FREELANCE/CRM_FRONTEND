297c7a23aa5b0d9e6955a241f8cefce86cbde340
RohithKrishna R <rohithkrishna3611@gmail.com>
2026-06-14 12:59:42 +0530

    New file structure

---
+++ src/Axios/axios.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+import { BACKEND_API } from "../constant/Static";
+
+export const axiosLogin = axios.create({
+  baseURL: BACKEND_API,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
---
+++ src/CommonCode/CommonFun.js
@@ -0,0 +1,16 @@
+import { axiosLogin } from "../Axios/axios";
+
+export const FetchAllAluminiDetail = async () => {
+  try {
+    const response = await axiosLogin.get("/training/alumini/fetchall");
+    const { success, data } = response.data;
+
+    //  Record exist
+    if (success !== 0) return data;
+    //  No Record found
+    return [];
+  } catch (error) {
+    console.error("FetchExistingPdf error:", error);
+    throw new Error(error?.response?.data?.message || "Failed to fetch PDFs");
+  }
+};
