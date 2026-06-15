fbe4805174106a001f2911fa76e202263949eca2
RohithKrishna R <rohithkrishna3611@gmail.com>
2026-06-16 00:08:12 +0530

    Side bar changed

--- src/Menu/menu.js
+++ src/Menu/menu.js
@@ -1,3 +1,4 @@
+import React from "react";
 import DashboardCustomizeIcon from "@mui/icons-material/DashboardCustomize";
 import PersonAddAlt1Icon from "@mui/icons-material/PersonAddAlt1";
 import GroupsIcon from "@mui/icons-material/Groups";
@@ -7,17 +8,43 @@ import SettingsSuggestIcon from "@mui/icons-material/SettingsSuggest";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import NotificationsActiveIcon from "@mui/icons-material/NotificationsActive";
 import EditDocumentIcon from "@mui/icons-material/EditDocument";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import WorkIcon from "@mui/icons-material/Work";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 export const Menu = [
   {
+    // groupLabel: "Main",
     label: "Dashboard",
-    icon: DashboardCustomizeIcon,
-    path: "",
+    icon: DashboardIcon,
+    path: "/dashboard",
   },
-  // SETTINGS
   {
+    label: "Calendar",
+    icon: CalendarMonthIcon,
+    path: "/calendar",
+  },
+  {
+    label: "Campaigns",
+    icon: WorkIcon,
+    nested: [
+      { label: "My Camp", path: "/campaigns/my-camp" },
+      { label: "Checklist", path: "/campaigns/checklist" },
+      { label: "Campers", path: "/campaigns/campers" },
+      { label: "Save Spots", path: "/campaigns/save-spots" },
+    ],
+  },
+  {
+    // groupLabel: "Account",
+    label: "Notification",
+    icon: NotificationsIcon,
+    path: "/notifications",
+  },
+  {
+    // groupLabel: "Account",
     label: "Settings",
     icon: SettingsSuggestIcon,
-    path: "settings",
+    path: "/home/settings",
   },
 ];
