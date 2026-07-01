11668d08a8391d7bc1ea57bf8fda1e56ab3d19e4
unknown <Administrator@MEDICITY.CO.IN>
2026-07-01 14:26:58 +0530

    User Info Chnaged

--- src/constant/Static.js
+++ src/constant/Static.js
@@ -1,6 +1,6 @@
 // export const BACKEND_API = "http://192.168.38.114:7000/api";
-export const BACKEND_API = "http://192.168.22.3:7000/api";
-export const BACKEND_IMAGE = "http://192.168.22.3:7000";
+export const BACKEND_API = "http://192.168.22.8:7000/api";
+export const BACKEND_IMAGE = "http://192.168.22.8:7000";
 
 
 // export const NAS_PATH = "C:\uploads\"
